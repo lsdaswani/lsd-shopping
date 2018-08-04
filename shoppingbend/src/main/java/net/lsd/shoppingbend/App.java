9d69299b20c7653450f88fe1cd130816027d6286
Lalsingh Daswani <lsd78622@gmail.com>
2018-08-04 15:35:53 +0530

    My Project Setup completed Successfully.

---
+++ shoppingbend/src/main/java/net/lsd/shoppingbend/App.java
@@ -0,0 +1,13 @@
+package net.lsd.shoppingbend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
